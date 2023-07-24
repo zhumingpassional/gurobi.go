6d0a7844b8b8abf7936922d71989f8dba2ead666
Ming Zhu <zhumingpassional@gmail.com>
2023-07-24 08:29:50 +0800

    Update cgoHelper.go

--- gurobi/cgoHelper.go
+++ gurobi/cgoHelper.go
@@ -1,5 +1,6 @@
 package gurobi
 
+
 // #cgo CXXFLAGS: --std=c++11 -I/Library/gurobi1002/macos_universal2/include
 
 // #cgo LDFLAGS: -L/Library/gurobi1002/macos_universal2/lib -lgurobi_c++ -lgurobi100
